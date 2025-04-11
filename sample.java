7a89198663b7801640a811b152b277d338d6d6d3
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-04-11 13:51:04 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -15,3 +15,53 @@ class Solution {
         return lvl + (startValue-curr);
     }
 }
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    int total =0;
+    public int depthSum(List<NestedInteger> nestedList) {
+        if(nestedList == null) {
+            return 0;
+        }
+        int lvl=1;
+        for (NestedInteger nestel: nestedList) {
+            dfs(nestel, lvl);
+        }
+        return total;
+    }
+    private void dfs(NestedInteger nestel, int depth) {
+        if(nestel.isInteger()) {
+            total = total + depth*nestel.getInteger();
+        } else {
+            for (NestedInteger el: nestel.getList()) {
+                dfs(el, depth+1);
+            }
+        }
+    }
+}
